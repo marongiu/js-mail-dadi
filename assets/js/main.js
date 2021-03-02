146b815398b632566bb6528e5d977f1a8f0b470d
Andrea <andreamarongiu05@gmail.com>
2021-03-02 14:57:23 +0100

    Inizio esercizio

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// Chiedi all'utente la sua email
+
+var email = prompt("Scrivi la tua email:");
+
+// Creo una lista di email ammesse
+
+var emailAccettate = ["andrearossi@gmail.com", "francescorossi@gmail.com",
+"valeriorossi@gmail.com", "annarossi@gmail.com"];
+
+
+for (var i = 0; i < emailAccettate.length; i++) {
+  emailAccettate[i]
+  console.log(emailAccettate);
+}

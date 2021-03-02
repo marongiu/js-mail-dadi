01503acc4bd6503efa8aa24fa37caad0c7fe39f7
Andrea <andreamarongiu05@gmail.com>
2021-03-02 15:31:21 +0100

    Fine esercizio email

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,25 @@
 // Chiedi all'utente la sua email
 
-var email = prompt("Scrivi la tua email:");
+var email = prompt("Scrivi la tua email: Le email accettate sono andrea@gmail.com, valerio@gmail.com, giacomo@gmail.com, maria@gmail.com");
 
 // Creo una lista di email ammesse
 
 var emailAccettate = ["andrea@gmail.com", "valerio@gmail.com", "giacomo@gmail.com", "maria@gmail.com",]
 
+// Creo la variabile seiPresente per inserirla in for
+seiPresente = false;
 // Controllo che l'email sia ammessa nella lista
+
 for (var i = 0; i < emailAccettate.length; i++) {
   if (emailAccettate[i] == email) {
-    console.log("L'email è accettata");
-  } 
+    // se l'email è presente ricreo successivamente una nuova condizione seiPresente == true
+    seiPresente = true;
+  }
+}
+
+// Se l'email è true l'email è accettata
+if (seiPresente == true) {
+  console.log("Sei ammesso");
+} else {
+  console.log("Non sei ammesso");
 }
